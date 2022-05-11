b4dcd1ce6dd7480036933a4494714ffe4fa9bfef
mubarek78 <shifamubarek@yahoo.com>
2022-05-10 19:19:01 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+        .then(resp => resp.json())
+        .then(data => document.querySelector('body').append(data['id']))
+        .catch(error => document.querySelector('body').append(error.message))
+
+}
